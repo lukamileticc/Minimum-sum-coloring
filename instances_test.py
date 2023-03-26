e7b09c95b3633846aa0b66f40efa5950b0995fa3
Luka <lukamilecki998@gmail.com>
2023-03-26 19:02:23 +0200

    Begin testing instance DSJC250.5.txt

--- instances_test.py
+++ instances_test.py
@@ -24,7 +24,7 @@ def draw_graph(title,path_to_save, xs, ys):
 
 if __name__ == '__main__':
     g = Graph()
-    file = 'fpsol2.i.2.txt'
+    file = 'DSJC250.5.txt'
     g.load_dimacs_file('graph_instances/group_2/' + file)
     # print(g)
     iteration_number = 5
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     MUTATION_PROB = 0.05  # 5%
 
     #parametri vns-a
-    k_max = 3
+    k_max = 5
     move_prob = 0.5
 
     random.seed(32452)
@@ -60,7 +60,7 @@ if __name__ == '__main__':
             best_value = curr_value
     end = time.time()
 
-    title = "Local search(iters=10000),\nGraph instance:" + file + '(451 8691),\nTime:' + str(
+    title = "Local search(iters=10000),\nGraph instance:" + file + '(250 15668),\nTime:' + str(
         round(end - start, 3)) + ' sec, Avg value:' + str(avg_value/iteration_number) + ' Best value:' + str(best_value)
     draw_graph(title, 'graphic_results/' + title + '.png', xs, ys)
 
@@ -81,7 +81,7 @@ if __name__ == '__main__':
             best_value = curr_value
     end = time.time()
 
-    title = "Simulated annealing(iters=10000),\nGraph instance:" + file + '(451 8691),\nTime:' + str(
+    title = "Simulated annealing(iters=10000),\nGraph instance:" + file + '(250 15668),\nTime:' + str(
         round(end - start, 3)) + ' sec, Avg value:' + str(avg_value/iteration_number) + ' Best value:' + str(best_value)
     draw_graph(title, 'graphic_results/' + title + '.png', xs, ys)
 
@@ -101,7 +101,7 @@ if __name__ == '__main__':
             best_value = curr_value
     end = time.time()
 
-    title = "Genetic algorithm(roulette selection),\nGraph instance:" + file + '(451 8691),\nTime:' + str(
+    title = "Genetic algorithm(roulette selection),\nGraph instance:" + file + '(250 15668),\nTime:' + str(
         round(end - start, 3)) + ' sec, Avg value:' + str(avg_value / iteration_number) + ' Best value:' + str(
         best_value)
     draw_graph(title, 'graphic_results/' + title + '.png', xs, ys)
@@ -122,7 +122,7 @@ if __name__ == '__main__':
             best_value = curr_value
     end = time.time()
 
-    title = "Hybrid(ga(roulette) + sa(iters=100)),\nGraph instance:" + file + '(451 8691),\nTime:' + str(
+    title = "Hybrid(ga(roulette) + sa(iters=100)),\nGraph instance:" + file + '(250 15668),\nTime:' + str(
         round(end - start, 3)) + ' sec, Avg value:' + str(avg_value / iteration_number) + ' Best value:' + str(
         best_value)
     draw_graph(title, 'graphic_results/' + title + '.png', xs, ys)
